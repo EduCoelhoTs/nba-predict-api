5591925d6e5755250340ea77e374ecbd5126443e
eduardo.coelho <eduardocoelhosilva12@gmail.com>
2026-02-14 11:27:28 -0300

    feat: validator pkg

---
+++ pkg/xvalidator/xvalidator.go
@@ -0,0 +1,27 @@
+package xvalidator
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+)
+
+var validate = validator.New(validator.WithRequiredStructEnabled())
+
+func ValidateStruct(s any) error {
+	var validationErrors validator.ValidationErrors
+	if err := validate.Struct(s); err != nil {
+		if errors.As(err, &validationErrors) {
+			var ResultError error
+			for _, err := range validationErrors {
+				errMsg := fmt.Errorf("field %s is invalid. Error: %s", err.Field(), err.Error())
+				ResultError = errors.Join(ResultError, errMsg)
+			}
+			return ResultError
+		}
+		return err
+	}
+
+	return nil
+}
